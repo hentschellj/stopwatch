78cc0f69232f7fab7119d4c7848b221991b4381b
hentschellj <hentschellj@gmail.com>
2020-11-17 22:29:57 -0600

    Added stop function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,44 +6,45 @@ let hours         = document.getElementById('hours'),
     currentTime,
     difference,
     timer         = 0,
-    interval
+    interval;
 
 const startButton = document.getElementById('start-button'),
       pauseButton = document.getElementById('pause-button'),
       stopButton  = document.getElementById('stop-button'),
-      resetButton = document.getElementById('reset-button')
+      resetButton = document.getElementById('reset-button');
 
 const start = function() {
-  setTime = Date.now()
-  interval = setInterval(update, 100)
+  setTime = Date.now();
+  interval = setInterval(update, 100);
 }
 
 const pause = function() {
-  clearInterval(interval)
+  clearInterval(interval);
 }
 
 const stop = function() {
-  console.log('Stopping')
+  clearInterval(interval);
+  timer = 0;
 }
 
 const reset = function() {
-  console.log('Resetting')
+  console.log('Resetting');
 }
 
 const update = function() {
-  currentTime = Date.now()
-  difference = currentTime - setTime
+  currentTime = Date.now();
+  difference = currentTime - setTime;
 
-  timer += difference
-  updateScreen()
-  setTime = currentTime
+  timer += difference;
+  updateScreen();
+  setTime = currentTime;
 }
 
 const updateScreen = function() {
-  milliseconds.innerText = timer/1000
+  milliseconds.innerText = timer/1000;
 }
 
-startButton.addEventListener('click', start)
-pauseButton.addEventListener('click', pause)
-stopButton.addEventListener('click', stop)
-resetButton.addEventListener('click', reset)
+startButton.addEventListener('click', start);
+pauseButton.addEventListener('click', pause);
+stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', reset);
